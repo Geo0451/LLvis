be6473fe6e5c912536df75ce4c537b663953ff41
Geo0451 <geokj0451@gmail.com>
2024-12-07 20:39:40 +0530

    Update 1
    Added more basic functions
    + Can now remove Nodes using index position, also support negative indices
    + Get length of linked list

--- exp.cpp
+++ exp.cpp
@@ -6,8 +6,7 @@
 
 
 
-
-#include <cstdio>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 
@@ -27,7 +26,6 @@ Node* convertWord(string w) //convert given word w into a LL with each letter as
     for (int i = 0; i < w.length(); i++ )
     {
         p->c = w[i]; //assign the letter
-        cout << w[i];
         if (i != w.length() - 1) // not the very last letter?
         {
             p->n = new Node; // address to next node is created
@@ -51,17 +49,89 @@ void displayLL(Node* fp) //displays an entire linked list given the first pointe
     }
 }
 
+
+int getLength(Node* fp) //get length of the linked list
+{
+    int c = 0;
+    while (fp != NULL) 
+    {
+        c++; 
+        fp = fp->n; 
+    }
+    return c;
+}
+
+
+Node* removeNthNode(int i,Node* p) //removes i-th node(zero-based w/ negative indexing)
+{                                  //and returns new first node(if applicable,else NULL)
+    int n = 0;
+    int len = getLength(p);
+    Node *prev,*next;
+    next = p->n;
+
+    if ((i > len - 1) || ( -i > len- 1)) //mod i is greater than last index position
+    {
+        return NULL; //eat 5-star, do nothing
+    }
+
+    if (i < 0) //to support negative indexing,
+    {
+        i = len + i; //we convert i into a positive index position
+    }
+
+    while (n < i)
+    {
+        prev = p; //predecessor pointer
+        p = next; //current pointer
+        next = p->n; //next pointer
+        n++;
+    }
+
+    delete p; //bye bye
+
+    //edge cases that require no stiching of pointers
+    if (i == 0) //first node
+    {
+        return next; //next is now your new first node
+    }
+    
+
+    //predecessor now points to successor instead of our now-deleted current pointer p
+    prev->n = next; 
+    return NULL;
+
+}
+
 int main()
 {
     string word;
+
+    system("clear");
+        
     cout << "Enter your word:\n" << endl;
     //cin >> word; //use getline because cin stops at first whitespace
     getline(cin,word);
+    Node* myp = convertWord(word);
 
-    displayLL(convertWord(word));
+    int ind;
+    Node* t; //test
+    while (true)
+    {
+        displayLL(myp);
+        cout << "Enter which letter to remove starting from 0: " << endl;
+        cin >> ind;
+
+        t = removeNthNode(ind, myp);
+        if (t != NULL)
+        {
+            myp = t;
+        }        
+        
+        cin.get();
+        //system("clear");
+    }
 
     
 
-    cout << "Done" << endl;
     return 0;
 }    
